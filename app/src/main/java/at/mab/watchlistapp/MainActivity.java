6b809cec35c6e8432119bfdc7a6068dec44c0ae0
DaHubbi <lucas.huber@gmx.at>
2017-11-14 16:23:48 +0100

    ADDED layout_register

--- app/src/main/java/at/mab/watchlistapp/MainActivity.java
+++ app/src/main/java/at/mab/watchlistapp/MainActivity.java
@@ -1,8 +1,8 @@
 package at.mab.watchlistapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 
