9878e6958a45ddea41a0b43ab5cf60b9de975f53
lolgame99 <lolgame99@gmail.com>
2017-11-28 15:01:01 +0100

    ADDED api request

--- app/src/main/java/at/mab/watchlistapp/PopularActivity.java
+++ app/src/main/java/at/mab/watchlistapp/PopularActivity.java
@@ -1,21 +1,58 @@
 package at.mab.watchlistapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ImageButton;
+import android.util.Log;
 
-/**
- * Created by bmaye on 07.11.2017.
- */
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class PopularActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_listitembig);
+        loadRestData();
+    }
+
+    private void loadRestData(){
+        try {
+            JsonArrayRequest request = new JsonArrayRequest("https://api.themoviedb.org/3/tv/popular?api_key=02315c61f82284303a120d89ce93baa4&language=de",
+                    new Response.Listener<JSONArray>() {
+                        @Override
+                        public void onResponse(JSONArray jsonArray) {
+                            for(int i = 0; i < jsonArray.length(); i++) {
+                                try {
+                                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                    Log.d("MAB",jsonObject.toString());
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+
+                            // allDone();
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
+                            Log.d("MAB",volleyError.getMessage());
+                            // Toast.makeText(MainActivity.this, "Unable to fetch data: " + volleyError.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+            Volley.newRequestQueue(this).add(request);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
